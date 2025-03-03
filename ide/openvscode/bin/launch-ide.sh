6fe59c037089e635424a9b0c47ec940a6bbbeb46
Struan Bartlett <struan.bartlett@NewsNow.co.uk>
2025-03-03 23:21:37 +0000

    Improve workspace settings.json

--- ide/openvscode/bin/launch-ide.sh
+++ ide/openvscode/bin/launch-ide.sh
@@ -23,10 +23,16 @@ log "Evaling arguments $@ ..."
 eval "$@"
 
 # Set needed environment variables
-export PATH="$IDE_PATH/bin:$PATH:$IDE_PATH/bin"
-export GIT_EXEC_PATH="$IDE_PATH/bin"
+export PATH="$PATH:$IDE_PATH/bin"
 
 log "Launching IDE from IIDE_PATH='$IIDE_PATH' using: ./node ./out/server-main.js --host 0.0.0.0 --port 3131 --without-connection-token ..."
+
+# Create system settings
+SETTINGS_DIR="$HOME/.openvscode-server/data/Machine"
+SETTINGS_FILE="$SETTINGS_DIR/settings.json"
+mkdir -p $SETTINGS_DIR
+[ -f "$SETTINGS_FILE" ] || echo '{}' >$SETTINGS_FILE
+jq --arg binpath "$IDE_PATH/bin" '."git.path"=$binpath+"/git" | ."telemetry.telemetryLevel"="off" | ."terminal.integrated.env.linux".PATH="${env:PATH}:"+$binpath' $SETTINGS_FILE >$SETTINGS_FILE.new && mv $SETTINGS_FILE.new $SETTINGS_FILE
 
 cd $IIDE_PATH/openvscode
 unset IDE_PATH IDE IIDE_PATH LOG_PATH
@@ -34,6 +40,4 @@ unset IDE_PATH IDE IIDE_PATH LOG_PATH
 log "- environment variables:"
 env | sed -r 's/^/    /' >&2
 
-# FIXME: Parametrise /opt/dockside here
-mkdir -p $HOME/.openvscode-server/data/Machine && echo '{ "git.path": "/opt/dockside/system/latest/bin/git", "git.confirmSync": false }' >$HOME/.openvscode-server/data/Machine/settings.json
 exec ./node ./out/server-main.js --host 0.0.0.0 --port 3131 --without-connection-token
